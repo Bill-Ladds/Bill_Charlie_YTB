813da885f988a1a75f68c600c81e0eca03cd152c
Bill <bill-ladds@live.nmit.ac.nz>
2023-03-22 11:24:43 +1300

    cleared the console

--- src/index.js
+++ src/index.js
@@ -7,39 +7,5 @@ console.log("Pair Coding")
  */
 console.log("Hey Bill")
 console.log("Hey Charlie, you forgot to add comments")
-//Just like this with the double slash my man
-/**
- * hi
- * this
- * is a multi
- * line comment yo
- */
-console.log("Loves a comment")
-/**
- * crazy man
- * you can straight up just add whatever in these bad boys
- * what a treat
- */
-console.log('sututututu')
-//get the dorito out
-/**
- * Brap
- * Brap
- * Brap
- * Brap
- */
-console.log("Just practice")
-//Don't mind me, just practicing the commands g
-console.log("looking forward to subway!")
-//Sheeeeesh that's made me hungry, might have to hit the dairy for lunch
-console.log("subway")
-/**
- * herb and cheese
- * riblet
- * cheese
- * sweet chilli mayo
- */
-console.log("IF ANYONE SEES THIS")
-//THIS IS JUST PRACTICE DONT JUDGE :)
-console.log("switch roles")
-//Taking over as Driver again
+//deleted comments to clear up console
+console.log("Cleared the console")
